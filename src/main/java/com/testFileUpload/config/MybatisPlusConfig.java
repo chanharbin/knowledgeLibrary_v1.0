a42c929a15f0bc46e657b76e54e0608d1eae257f
hzgzm0210 <2764313791@qq.com>
2019-07-24 12:52:33 +0800

    1234

--- src/main/java/com/testFileUpload/config/MybatisPlusConfig.java
+++ src/main/java/com/testFileUpload/config/MybatisPlusConfig.java
@@ -1,18 +1,16 @@
 package com.testFileUpload.config;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MybatisPlusConfig {
-    /**
-     * 分页插件
-     */
+@MapperScan("com.testFileUpload")
+public class MyBatisPlusConfig {
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor page = new PaginationInterceptor();
-        page.setDialectType("mysql");
-        return page;
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
     }
 }
